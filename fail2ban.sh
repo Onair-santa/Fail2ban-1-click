aabf6cae63edffd32e00370aaddf4eb513dab8ef
Onair <42511409+Onair-santa@users.noreply.github.com>
2024-04-08 06:36:17 +0500

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -46,9 +46,9 @@ sleep 0.5
 # Open default ports.
 sudo nft add rule inet filter input iifname lo accept
 sudo nft add rule inet filter input ct state established,related accept
-sudo nft add rule inet filter input iifname $INTERFACE tcp dport 22 accept
-sudo nft add rule inet filter input iifname $INTERFACE tcp dport 80 accept
-sudo nft add rule inet filter input iifname $INTERFACE tcp dport 443 accept
+sudo nft add rule inet filter input iifname "$INTERFACE" tcp dport 22 accept
+sudo nft add rule inet filter input iifname "$INTERFACE" tcp dport 80 accept
+sudo nft add rule inet filter input iifname "$INTERFACE" tcp dport 443 accept
 sudo nft add chain inet filter input '{ policy drop; }'
 sleep 0.5
 echo '#!/usr/sbin/nft -f' > /etc/nftables.conf
@@ -71,12 +71,36 @@ echo
 yellow_msg 'Installing Fail2ban...'
 echo
 sleep 0.5
-    
-F2B_PATH="/etc/fail2ban/jail.local"
+
 wget https://github.com/fail2ban/fail2ban/releases/download/1.0.2/fail2ban_1.0.2-1.upstream1_all.deb
 sudo dpkg -i fail2ban_1.0.2-1.upstream1_all.deb
 sleep 1
-wget "https://raw.githubusercontent.com/Onair-santa/Fail2ban-1-click/main/jail.local" -q -O $F2B_PATH
+cat >/etc/fail2ban/jail.local <<-\EOF
+[DEFAULT]
+bantime.increment = true
+bantime.rndtime = 10m
+bantime.factor = 1
+bantime.formula = ban.Time * (1<<(ban.Count if ban.Count<20 else 20)) * banFactor
+bantime.multipliers = 1 5 30 60 300 720 1440 2880
+ignoreself = true
+ignoreip = 127.0.0.1/8
+bantime  = 1h
+findtime  = 10m
+maxretry = 3
+banaction = nftables[type=multiport]
+banaction_allports = nftables[type=allports]
+[sshd]
+enabled = true
+port    = 22
+logpath = %(sshd_log)s
+backend = %(sshd_backend)s
+[recidive]
+enabled = true
+logpath = /var/log/fail2ban.log
+banaction = %(banaction_allports)s
+bantime = 1w
+findtime = 1d
+EOF
 sudo systemctl enable fail2ban
 fail2ban-client reload
 sleep 1
